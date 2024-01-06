f50b09d17cfd371a3b2557edca0a41ff08fab3fc
Himanshu <phimanshu925@gmail.com>
2024-01-06 23:57:04 +0530

    JSON Data

--- ProductApp/PRODApp/obj/Debug/net8.0/PRODApp.AssemblyInfo.cs
+++ ProductApp/PRODApp/obj/Debug/net8.0/PRODApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRODApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634c3acefc392529ee4e3e15f5f8c923de9f5401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df387c9bf4025e158da536813ccbff33de7bf21")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRODApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRODApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
